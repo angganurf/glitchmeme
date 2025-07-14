673cdadbf2d988d4e663e5ad53bb8b8d2365dd49
angganurf <angganurfaizal234@gmail.com>
2025-07-15 00:11:03 +0700

    set base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://glitchmeme.wtf/",
+  base: "/glitchmeme/",
   build: {
     sourcemap: false,  // Disable sourcemaps
   }
