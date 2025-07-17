d89f3c74c4e715d5bd96909233ca9b2e96fb1a68
angganurf <angganurfaizal234@gmail.com>
2025-07-17 09:03:10 +0700

    no cache

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { hash } from './src/utils/functions.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,14 @@ export default defineConfig({
   base: "https://glitchmeme.wtf/",
   build: {
     sourcemap: false,  // Disable sourcemaps
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]` + hash + `.js`,
+        chunkFileNames: `[name]` + hash + `.js`,
+        assetFileNames: `[name]` + hash + `.[ext]`
+      }
+    }
   }
 })
+
+// vite.config.js
